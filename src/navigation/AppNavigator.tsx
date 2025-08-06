af1ba4906f9174c84ce1d0e2d9e3944e915a0a1d
Mustafa OZ <mustafaaozz@gmail.com>
2025-08-06 17:05:34 +0300

    feat: create AppNavigator component

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { RootStackParamList } from '../types';
+import { ProductListScreen } from '../screens';
+import { CartHeaderButton } from '../components';
+import { colors } from '../constants';
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const AppNavigator: React.FC = () => {
+  const headerRightComponent = useCallback(() => <CartHeaderButton />, []);
+
+  return (
+    <Stack.Navigator
+      initialRouteName='ProductList'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+        headerRight: headerRightComponent,
+      }}
+    >
+      <Stack.Screen
+        name='ProductList'
+        component={ProductListScreen}
+        options={{
+          title: 'Products',
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator;
